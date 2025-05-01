3223463d7e95d791579072cee47638f9cb162ad8
yaser <yaser-bahaa@hotmail.com>
2025-05-01 16:41:16 +0300

    res

--- src/comp/sec2.jsx
+++ src/comp/sec2.jsx
@@ -16,26 +16,34 @@ function Sec2() {
   const box2 = useRef(null)
   const box3 = useRef(null)
   const box4 = useRef(null)
-  const test = useRef(null)
 
 
   useEffect(()=>{
 
 
-    gsap.to(box1.current, {
+
+
+    requestAnimationFrame(() => {
+          gsap.to(box1.current, {
       scrollTrigger: {
         trigger: box1.current,
         start: '0px 90%', // Trigger the animation when 20% from the top of the container hits 50% of the viewport
-        end: '-100px 90%', // End the animation when top of the container is 25% from the top of the viewportop of the viewport
+        end: '100px 90%', // End the animation when top of the container is 25% from the top of the viewportop of the viewport
     scrub: 1, // Smooth scrubbing, takes 1 second to "catch up" to the scrollbar
     markers:true// Show start and end markers (for debugging)
   },
   // y: "3400px", // Animate the box 300px to the right
   // y: "2300px", // Animate the box 300px to the right
-  x: window.innerWidth > 515 ? "-90px" : "0px",  ease: 'none',
+  x: window.innerWidth > 515 ? "-90px" : "0px",  
+  ease: 'none',
   // duration: 2,
   });
 
+})
+
+
+
+  window.onload = function () {
   gsap.to(box2.current, {
     scrollTrigger: {
       trigger: box2.current,
@@ -46,10 +54,14 @@ function Sec2() {
 },
 // y: "3400px", // Animate the box 300px to the right
 // y: "2300px", // Animate the box 300px to the right
-x: window.innerWidth > 515 ? "-90px" : "0px",ease: 'none',
+x: window.innerWidth > 515 ? "-90px" : "0px",
+ease: 'none',
 // duration: 2,
 });
 
+  }
+
+window.onload = function () {
 gsap.to(box3.current, {
   scrollTrigger: {
     trigger: box3.current,
@@ -60,10 +72,15 @@ scrub: 1, // Smooth scrubbing, takes 1 second to "catch up" to the scrollbar
 },
 // y: "3400px", // Animate the box 300px to the right
 // y: "2300px", // Animate the box 300px to the right
-x: window.innerWidth > 515 ? "-90px" : "0px",ease: 'none',
+x: window.innerWidth > 515 ? "-90px" : "0px",
+ease: 'none',
 // duration: 2,
 });
 
+}
+
+
+window.onload = function () {
 gsap.to(box4.current, {
   scrollTrigger: {
     trigger: box4.current,
@@ -79,8 +96,9 @@ x: window.innerWidth > 515 ? "-90px" : "0px",
 ease: 'none',
 // duration: 2,
 });
+}
 
-
+window.onload = function () {
   gsap.to(textRef.current, {
     scrollTrigger: {
       trigger: textRef.current,
@@ -95,13 +113,16 @@ y: "960px",
 ease: 'none',
 // duration: 2,
 });
+}
 
 
   },[])
 
+
+
   return (
     <>
-    <div ref={test} className='sec2Parent'>
+    <div className='sec2Parent'>
     <h1 className='mobileTitle'>What makes us stand out?</h1>
     <div className='sec2TitleCont'>
 
