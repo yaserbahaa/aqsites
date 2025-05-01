e69ea49b558e235203702211f2219efe179ec281
yaser <yaser-bahaa@hotmail.com>
2025-05-01 17:20:47 +0300

    res

--- src/comp/ourwork.jsx
+++ src/comp/ourwork.jsx
@@ -28,6 +28,13 @@ function Ourwork() {
 const themes = useRef(null)
 const textRef = useRef(null)
 useEffect(()=>{
+  
+// Add a delay before forcing a refresh
+setTimeout(() => {
+  ScrollTrigger.refresh();
+}, 250); // Delay helps layout fully settle
+
+
 
   gsap.to(themes.current, {
     scrollTrigger: {
